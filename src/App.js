e2faa0087748cb3d369fda49ad96e803e611a972
Ryan Burns <ryankburns@gmail.com>
2022-06-25 22:10:36 -0700

    added search params component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Bentley" animal="Dog" breed="French Bulldog" />
-      <Pet name="Nala" animal="Dog" breed="French Bulldog" />
-      <Pet name="Metro" animal="Cat" breed="Mix" />
+      <SearchParams />
     </div>
   );
 };
